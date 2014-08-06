47c06161c6bd5bc4ab3756a46200c9e734330ccb
Rob Day <Robert.Day@metaswitch.com>
2014-08-06 11:55:08 +0100

    Allow reuse of the same specific ID

--- src/metaswitch/ellis/api/numbers.py
+++ src/metaswitch/ellis/api/numbers.py
@@ -305,7 +305,11 @@ class NumberHandler(_base.LoggedInHandler):
         pstn = self.get_argument('pstn', 'false') == 'true'
         private_id = self.get_argument('private_id', None)
         new_private_id = self.get_argument('new_private_id', None)
-        number_id = numbers.add_number_to_pool(db_sess, sip_uri, False)
+        try:
+            number_id = numbers.get_sip_uri_number_id(db_sess, sip_uri)
+        except NotFound:
+            # This SIP URI is not currently in the pool, so add it
+            number_id = numbers.add_number_to_pool(db_sess, sip_uri, False)
         numbers.allocate_specific_number(db_sess, user_id, number_id)
         self.sip_uri = sip_uri
         db_sess.commit()
