bba9ec77af2136539bdfcaf29ccc62234d256383
Eleanor Merry <eleanor.merry@metaswitch.com>
2016-01-19 16:27:13 +0000

    Fix up basic runtime errors

--- scripts/repop_hs.py
+++ scripts/repop_hs.py
@@ -45,14 +45,6 @@ from metaswitch.ellis import settings
 from metaswitch.common import utils
 from metaswitch.common import ifcs
 
-def get_digest(user, callback):
-    url = homestead.digest_url(user, user)
-    homestead.fetch(url, callback, method="GET")
-
-def get_filter_criteria(user, callback):
-    url = homestead.filter_url(user)
-    homestead.fetch(url, callback, method="GET")
-
 num_requests = 0
 num_responses = 0
 errors = 0
@@ -61,7 +53,7 @@ def standalone():
     global num_responses, num_requests
 
     db_sess = connection.Session()
-    c = db_sess.execute("SELECT number FROM numbers;")
+    c = db_sess.execute("SELECT number FROM numbers where OWNER_ID is not NULL;")
 
     def inc_resp_count():
         global num_responses, num_requests
@@ -98,7 +90,7 @@ def standalone():
             if response.code // 100 != 2:
                 print >> sys.stderr, "%s Bad response %s" % (sip_uri, response)
                 errors += 1
-            get_filter_criteria(sip_uri, check_callback)
+            homestead.get_filter_criteria(sip_uri, check_callback)
 
         def get_digest_callback(response, sip_uri=sip_uri):
             """
@@ -109,7 +101,7 @@ def standalone():
             print "%s Get digest response %s" % (sip_uri, response.code)
             if response.code == 404:
                 print "%s digest needs to be repopulated" % (sip_uri,)
-                homestead.post_password(sip_uri, sip_uri, utils.generate_sip_password(), post_digest_callback)
+                homestead.put_password(sip_uri, sip_uri, utils.generate_sip_password(), post_digest_callback)
             else:
                 inc_resp_count()
 
@@ -123,7 +115,7 @@ def standalone():
             if response.code // 100 != 2:
                 print >> sys.stderr, "%s Bad response %s" % (sip_uri, response)
                 errors += 1
-            get_digest(sip_uri, check_callback)
+            homestead.get_digest(sip_uri, check_callback)
 
         def check_callback(response, sip_uri=sip_uri):
             """
@@ -137,8 +129,8 @@ def standalone():
             inc_resp_count()
 
         num_requests += 2
-        get_digest(sip_uri, get_digest_callback)
-        get_filter_criteria(sip_uri, get_ifc_callback)
+        homestead.get_digest(sip_uri, get_digest_callback)
+        homestead.get_filter_criteria(sip_uri, get_ifc_callback)
     print "Finished queuing requests."
 
 
